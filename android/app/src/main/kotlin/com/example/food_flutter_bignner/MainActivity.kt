eeadd2bd6fd1377c765e1e9e604f9e8b31a9809e
emetdas <emet@b2bleadstaff.com>
2020-12-16 11:07:00 +0600

    flutter project

---
+++ android/app/src/main/kotlin/com/example/food_flutter_bignner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_flutter_bignner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FoodApp());
+}
+
+class FoodApp extends StatelessWidget {
+  const FoodApp({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
